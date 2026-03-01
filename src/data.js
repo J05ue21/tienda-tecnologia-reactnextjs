d382b2b256778e768448b43936cbe6927e91f811
J05ue21 <orellana3566@gmail.com>
2026-02-28 18:05:10 -0600

    fichero data.js que contiene datos de articulos y un par de usuarios

---
+++ src/data.js
@@ -0,0 +1,192 @@
+export const data = [
+    {
+        id: 1,
+        title: 'Laptop Gaming ASUS ROG',
+        price: 1200,
+        urlImage: 'https://m.media-amazon.com/images/I/71z0om-Cz1L._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: 'Monitor 27" 4K LG',
+        price: 350,
+        urlImage: 'https://m.media-amazon.com/images/I/71XWwdw-TcL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: 'Teclado Mecánico RGB',
+        price: 85,
+        urlImage: 'https://m.media-amazon.com/images/I/61cdNnPM7DL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: 'Mouse Inalámbrico Logitech MX Master 3S',
+        price: 99,
+        urlImage: 'https://m.media-amazon.com/images/I/61ni3t1ryQL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 5,
+        title: 'Procesador Intel Core i9-14900K',
+        price: 580,
+        urlImage: 'https://m.media-amazon.com/images/I/61jRMCAX4CL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 6,
+        title: 'Memoria RAM DDR5 32GB Corsair Vengeance',
+        price: 130,
+        urlImage: 'https://m.media-amazon.com/images/I/61D2DDpDITL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 7,
+        title: 'Disco Duro SSD NVMe 2TB Samsung 990 Pro',
+        price: 175,
+        urlImage: 'https://m.media-amazon.com/images/I/819Y6SlpTWL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 8,
+        title: 'Silla Gamer Ergonómica Secretlab',
+        price: 450,
+        urlImage: 'https://m.media-amazon.com/images/I/517HZjvB42L._AC_SL1000_.jpg',
+        quantity: 1
+    },
+    {
+        id: 9,
+        title: 'Audífonos HyperX Cloud II Wireless',
+        price: 120,
+        urlImage: 'https://m.media-amazon.com/images/I/61e0+8QzVBL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 10,
+        title: 'Webcam Razer Kiyo Pro 1080p',
+        price: 110,
+        urlImage: 'https://m.media-amazon.com/images/I/71svP8z5CuL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 11,
+        title: 'Licencia Microsoft Office 2021 Pro',
+        price: 250,
+        urlImage: 'https://m.media-amazon.com/images/I/41H8-1paozL._AC_SL1000_.jpg',
+        quantity: 1
+    },
+    {
+        id: 12,
+        title: 'Suscripción Adobe Creative Cloud 1 Año',
+        price: 600,
+        urlImage: 'https://m.media-amazon.com/images/I/81aR6zzv+9L._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 13,
+        title: 'Placa Base ASUS ROG Strix Z790-E',
+        price: 420,
+        urlImage: 'https://m.media-amazon.com/images/I/81cQQqzPcSL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 14,
+        title: 'Fuente de Poder Corsair RM850x 80+ Gold',
+        price: 145,
+        urlImage: 'https://m.media-amazon.com/images/I/71dj+5GQwEL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 15,
+        title: 'Sistema de Enfriamiento Líquido Kraken Elite',
+        price: 280,
+        urlImage: 'https://m.media-amazon.com/images/I/71VXiKxAutL._SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 16,
+        title: 'Micrófono Shure SM7B',
+        price: 399,
+        urlImage: 'https://m.media-amazon.com/images/I/71LYHO3+wnL._AC_SL1356_.jpg',
+        quantity: 1
+    },
+    {
+        id: 17,
+        title: 'Stream Deck MK.2 Elgato',
+        price: 150,
+        urlImage: 'https://m.media-amazon.com/images/I/61gtdFnK+UL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 18,
+        title: 'Gabinete NZXT H9 Flow White',
+        price: 160,
+        urlImage: 'hhttps://m.media-amazon.com/images/I/61Yr06ThzYL._AC_SL1000_.jpg',
+        quantity: 1
+    },
+    {
+        id: 19,
+        title: 'Router ASUS WiFi 6 GT-AX11000',
+        price: 330,
+        urlImage: 'https://m.media-amazon.com/images/I/615qUbfxukL._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 20,
+        title: 'Tarjeta Gráfica RTX 4070 Ti Super',
+        price: 799,
+        urlImage: 'hhttps://m.media-amazon.com/images/I/81iw1lPCN5L._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 21,
+        title: 'Antivirus Norton 360 - 3 Dispositivos',
+        price: 45,
+        urlImage: 'https://m.media-amazon.com/images/I/71Kjmo0B5+L._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 22,
+        title: 'Capture Card Elgato HD60 X',
+        price: 180,
+        urlImage: 'https://m.media-amazon.com/images/I/51M6As4xKlL._AC_SL1000_.jpg',
+        quantity: 1
+    },
+    {
+        id: 23,
+        title: 'iPad Pro M4 11"',
+        price: 1999,
+        urlImage: 'https://m.media-amazon.com/images/G/01/apparel/rcxgs/tile._CB483369110_.gif',
+        quantity: 1
+    },
+    {
+        id: 24,
+        title: 'Docking Station Thunderbolt 4',
+        price: 210,
+        urlImage: 'https://m.media-amazon.com/images/I/61ENfuME79L._AC_SL1500_.jpg',
+        quantity: 1
+    },
+    {
+        id: 25,
+        title: 'Kit de Herramientas iFixit Pro',
+        price: 75,
+        urlImage: 'https://m.media-amazon.com/images/I/910pxflmH4L._AC_SL1500_.jpg',
+        quantity: 1
+    }
+];
+
+export const users = [
+    {
+        id: 1,
+        username: 'E.Josue',
+        password: 'orellana26',
+        name: 'Eduardo Ortiz'
+    },
+    {
+        id: 2,
+        username: 'MariEunice',
+        password: 'shop_eu_2026',
+        name: 'Marielos Orellana'
+    }
+]
